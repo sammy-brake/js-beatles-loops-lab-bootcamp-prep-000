e491626fa738b210889f35e54a7cc805ca03c28e
Samantha brake <sammy.brake@gmail.com>
2018-02-09 22:39:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,35 @@
+function theBeatlesPlay(musicians, instruments) {
+  var emptyArray = [];
+  
+  for(var i = 0; i < 4; i++) {
+    
+    emptyArray.push(`${musicians[i]} plays ${instruments[i]}`) ;
+    
+  }
+  
+  return emptyArray ;
+}
+
+function johnLennonFacts(array) {
+   let n = 0;
+   var text = "!!!" ;
+   
+  while (n < array.length) { 
+            
+            
+     return (array[n] + text) ; 
+          
+   
+    
+        
+  }
+     
+
+
+}
+
+function iLoveTheBeatles(n) {
+  var array = [] ; 
+  
+  do
+}
