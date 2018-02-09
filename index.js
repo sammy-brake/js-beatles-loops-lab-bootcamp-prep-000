53e9d281c45241cbdf993d6c9dd074231fdeab5d
Samantha brake <sammy.brake@gmail.com>
2018-02-09 22:47:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,13 +13,7 @@ function theBeatlesPlay(musicians, instruments) {
 function johnLennonFacts() {
    let n = 0;
    var newArray = [] ; 
-   const array = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
-   
+  
   while (n < array.length) { 
     
             array.push(`${array[n]}!!!`) ; 
